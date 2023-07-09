6d702a59cdac08b479a713ac46c75c2c3bb41dea
Zyo7 <franki.tonnelier@gmail.com>
2023-07-09 08:20:04 +0200

    Commit de la deuxième étape du projet

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,13 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let arrowLeft = document.getElementById("arrow_left");
+arrowLeft.addEventListener("click", function () {
+	console.log("Vous avez cliqué sur la flèche gauche")
+});
+
+let arrowRight = document.getElementById("arrow_right");
+arrowRight.addEventListener("click", function () {
+	console.log("Vous avez cliqué sur la flèche droite")
+});
